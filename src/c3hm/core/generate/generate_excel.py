da56239daa0254274e8c654db9fb22f7c8123e2b
Vincent Archambault-Bouffard <varchambaultbouffard@cmaisonneuve.qc.ca>
2025-04-21 00:20:10 -0400

    XL getting better

--- src/c3hm/core/generate/generate_excel.py
+++ src/c3hm/core/generate/generate_excel.py
@@ -19,8 +19,10 @@ def generate_excel_from_rubric(rubric: Rubric, output_path: Path | str) -> None:
 
     # Tailles de colonnes
     ws.column_dimensions['A'].width = 50
-    for i in range(2, 10):
+    ws.column_dimensions['B'].width = 6
+    for i in range(3, 11):
         ws.column_dimensions[pyxl_utils.get_column_letter(i)].width = 15
+    ws.column_dimensions[pyxl_utils.get_column_letter(11)].width = 60
 
     # Titre
     ws.append([rubric.title()])
@@ -28,34 +30,63 @@ def generate_excel_from_rubric(rubric: Rubric, output_path: Path | str) -> None:
     cell.style = "Title"
     ws.append([])
 
+    # Total sur X pts
+    pts = "pt" if rubric.grid.total_score == 1 else "pts"
+    ws.append([f"Total sur {rubric.grid.total_score} {pts}"])
+    cell = ws.cell(row=ws.max_row, column=1)
+    cell.style = "Headline 2"
+
+    cell = ws.cell(row=ws.max_row, column=2)
+    cell.style = "Calculation"
+
+    # Grille
     grid = rubric.grid
     scale_colors = scale_color_schemes(len(grid.scale))
 
-    # En-tête
-    ws.append([''] + grid.scale)
-    for i in range(2, len(grid.scale) + 2):
+    # En-tête - seuils
+    col_before_scale = 2
+    ws.append([''] * col_before_scale + grid.thresholds)
+    for i in range(col_before_scale + 1, len(grid.thresholds) + col_before_scale + 1):
+        cell = ws.cell(row=ws.max_row, column=i)
+        cell.style = "Explanatory Text"
+
+    # En-tête - barème
+    extra_cols = ["note calculée", "note manuelle", "note en pts", "commentaires"]
+    ws.append([''] * col_before_scale + grid.scale + extra_cols)
+    for i in range(col_before_scale + 1, len(grid.scale) + col_before_scale + 1 + len(extra_cols)):
         cell = ws.cell(row=ws.max_row, column=i)
         cell.style = "Headline 4"
-        if scale_colors:
-            color = scale_colors[i - 2]
+        if scale_colors and i - col_before_scale - 1 < len(scale_colors):
+            color = scale_colors[i - col_before_scale - 1]
             cell.fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
 
 
     for criterion in grid.criteria:
         # Critère
-        ws.append([criterion.name])
+        ws.append([criterion.name, criterion.weight])
         cell = ws.cell(row=ws.max_row, column=1)
         cell.style = "Headline 3"
+
+        cell = ws.cell(row=ws.max_row, column= col_before_scale + len(grid.scale) + 1)
+        cell.style = "Calculation"
+
+        cell = ws.cell(row=ws.max_row, column= col_before_scale + len(grid.scale) + 2)
+        cell.style = "Input"
+
+        cell = ws.cell(row=ws.max_row, column= col_before_scale + len(grid.scale) + 3)
+        cell.style = "Calculation"
+
         # Descripteur
         for indicator in criterion.indicators:
-            ws.append([indicator.name])
-            cell = ws.cell(row=ws.max_row, column=1)
-            cell.style = "Explanatory Text"
+            ws.append([indicator.name, 1])
+            for i in range(2):
+                cell = ws.cell(row=ws.max_row, column=i+2)
+                cell.style = "Explanatory Text"
             for i in range(len(indicator.descriptors)):
                 color = "D3D3D3"  # Gris clair
                 if scale_colors:
                     color = scale_colors[i]
-                col_letter = pyxl_utils.get_column_letter(i + 2)
+                col_letter = pyxl_utils.get_column_letter(i + col_before_scale + 1)
                 rule = FormulaRule(
                     formula=[f'NOT(ISBLANK({col_letter}{ws.max_row}))'],
                     stopIfTrue=True,
