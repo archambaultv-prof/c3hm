0666cd61617e067898ee819026f12b8868fc9fd5
Vincent Archambault-Bouffard <varchambaultbouffard@cmaisonneuve.qc.ca>
2025-12-28 00:44:58 -0500

    Does not overwrite existing file by default

--- src/c3hm/cli/template.py
+++ src/c3hm/cli/template.py
@@ -17,14 +17,23 @@ from c3hm.commands.template import export_template
     default=None,
     help="Chemin vers la grille d'évaluation à générer"
 )
+@click.option(
+    '--force', '-f', 'force',
+    is_flag=True,
+    default=False,
+    help="Force l'écrasement du fichier de sortie s'il existe"
+)
 
-def template_command(output_path: Path) -> None:
+def template_command(output_path: Path, force: bool) -> None:
     """
     Génère une grille d'évaluation.
     """
+    original_output_path = output_path
     if output_path is None:
         output_path = Path.cwd() / "grille.yaml"
     elif not output_path.is_absolute():
         output_path = Path.cwd() / output_path
 
+    if output_path.exists() and not force:
+        raise FileExistsError(f"Le fichier {original_output_path} existe déjà. Utilisez -f pour l'écraser.")
     export_template(output_path)
